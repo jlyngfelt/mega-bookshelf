fe331f9f3944a34ee4ddfaea73da76deb6511f18
anna-dahlberg <anna.dahlberg@live.se>
2025-03-13 10:18:37 +0100

    created array for books

---
+++ CookBooks.php
@@ -0,0 +1,14 @@
+<?php
+
+$cookbooks = [
+    ["title" => "Salt, Fat, Acid, Heat", "author" => "Samin Nosrat", "theme" => "Cooking fundamentals"],
+    ["title" => "The Food Lab", "author" => "J. Kenji López-Alt", "theme" => "Science-based cooking"],
+    ["title" => "Joy of Cooking", "author" => "Irma S. Rombauer", "theme" => "Classic American cooking"],
+    ["title" => "Essentials of Classic Italian Cooking", "author" => "Marcella Hazan", "theme" => "Italian"],
+    ["title" => "Japan: The Cookbook", "author" => "Nancy Singleton Hachisu", "theme" => "Japanese"],
+    ["title" => "Mexican Everyday", "author" => "Rick Bayless", "theme" => "Mexican"],
+    ["title" => "The Nordic Cookbook", "author" => "Magnus Nilsson", "theme" => "Nordic cuisine"],
+    ["title" => "Dishoom: From Bombay with Love", "author" => "Shamil Thakrar", "theme" => "Indian"],
+    ["title" => "Oaxaca: Home Cooking from the Heart of Mexico", "author" => "Bricia Lopez", "theme" => "Regional Mexican"],
+    ["title" => "Vegetable Kingdom", "author" => "Bryant Terry", "theme" => "Plant-based cooking"]
+];
