2a0639dc39ddab2ae6a58544e76af6f7cee3e4ff
maxjvjohansson <max.jv.johansson@gmail.com>
2025-03-13 10:05:12 +0100

    Added boilerplate HTML structure for the landingpage within index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mega Bookshelf</title>
+    <link rel="stylesheet" href=""> <!-- Fill this with correct path to CSS file -->
+</head>
+
+<body>
+
+    <header>
+        <h1>Welcome to the Mega Bookshelf</h1>
+    </header>
+
+    <main>
+        <section class="bookshelf"> <!-- Container for bookshelf. Book foreach goes within section tags -->
+
+        </section>
+    </main>
+
+    <footer>
+
+    </footer>
+
+    <script src=""></script> <!-- Fill this with correct path to JS file -->
+
+</body>
+
+</html>
